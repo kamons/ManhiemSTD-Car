924f8b8e27d6ae9d1fbd461da9243fe3ce17472f
KamolSTD <kamons@yahoo.com>
2016-10-23 16:12:15 +0700

    Finish 23-10.2016

--- app/src/main/java/manheim/kamolstd/manheimcar/MainActivity.java
+++ app/src/main/java/manheim/kamolstd/manheimcar/MainActivity.java
@@ -1,16 +1,21 @@
 package manheim.kamolstd.manheimcar;
 
 import android.content.Intent;
+import android.security.keystore.UserNotAuthenticatedException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
 
     // Explicit  การประกาสศ ตัวแปล
     private Button singInButton, signUpButton;
+    private EditText userEditText, passwordEditText;
+
+
 
 
     @Override
@@ -21,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         // Bind Widget
         signUpButton = (Button) findViewById(R.id.button2);
         singInButton = (Button) findViewById(R.id.button);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+
 
         //signUp Controller
         signUpButton.setOnClickListener(new View.OnClickListener() {
